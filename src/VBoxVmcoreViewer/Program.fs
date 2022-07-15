32b7980979acdbac745fc331a20ae7b7dc5cd415
Maksim Kislyakov <mail@kmaksim.dev>
2022-07-15 13:35:23 +0300

    Add function (getPagingTables) to read paging tables of cpu dump.

--- src/VBoxVmcoreViewer/Program.fs
+++ src/VBoxVmcoreViewer/Program.fs
@@ -3,32 +3,37 @@ open VBoxVmcoreViewer.BinaryOps.Operations
 open VBoxVmcoreViewer.BinaryOps.Types
 open VBoxVmcoreViewer.LibElf.ElfFile
 open VBoxVmcoreViewer.LibElf.PrettyPrint
-open VBoxVmcoreViewer.X86.X86
 open VBoxVmcoreViewer.LibElf.Types
+open VBoxVmcoreViewer.X86.X86
 
-let readAndPrintHeader file =
+let readAndPrintFile file =
     try
-        use fstream = File.OpenRead (file)
+        use fstream = File.OpenRead file
         let stream = { Endianess = nativeEndianess; Stream = fstream }
         let elfFile = readElfFile stream
 
         match elfFile with
         | Ok elfFile ->
             printfn $"%s{file}"
-
             printfn $"%A{elfFile.Header}"
-
-            printfn $"Notes (%d{List.length elfFile.Notes}):"
-            List.iter (printfn "%A") elfFile.Notes
 
             printfn $"Program headers (%d{List.length elfFile.ProgramHeaders}):"
             printfn $"%s{prettyPhHeader}"
             List.iter (fun header -> printfn $"%s{prettyPh header}") elfFile.ProgramHeaders
 
-            List.iter (fun note ->
-                match note with
-                | { Note.Type = NoteType.VBoxCpu cpuDump } -> printfn $"Paging mode: %A{getPadingMode cpuDump}"
-                | _ -> ())  elfFile.Notes
+            printfn "VBoxCore: "
+            elfFile.Notes
+            |> List.filter (fun n -> match n.Type with NoteType.VBoxCore _ -> true | _ -> false)
+            |> List.iter (printfn "%A")
+
+            printfn "CPU paging modes: "
+            elfFile.Notes
+            |> List.choose (fun n -> match n.Type with VBoxCpu dump -> Some dump | _ -> Option.None)
+            |> List.map (fun dump -> getPagingTables dump elfFile)
+            |> List.iter (fun result ->
+                match result with
+                | Ok startOfPaging -> printfn $"%A{startOfPaging.Mode}"
+                | Error err -> printfn $"%s{err}")
 
             0
         | Error msg ->
@@ -42,7 +47,7 @@ let readAndPrintHeader file =
 [<EntryPoint>]
 let main argv =
     match argv with
-    | [| file |] when File.Exists file -> readAndPrintHeader file
+    | [| file |] when File.Exists file -> readAndPrintFile file
     | _ ->
         printfn "Usage: <path-to-elf-file>"
         2
